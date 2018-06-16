f980583b3e50546808113b5515da07a56cc029fe
Neo <neo@Neos-MacBook-Pro.local>
2018-06-16 11:34:35 +0800

    update sync test

--- main_test.go
+++ main_test.go
@@ -41,20 +41,8 @@ func Test_Sync(t *testing.T) {
 		return
 	}
 
-	//5,468,417
 	for index := 5790795 + 1; index < int(getBcBlockNumber())-15; index++ {
 		SyncBlock(pgEngine, int64(index))
-		//fmt.Println("result: ", b)
-		//return
-		// asset := "KEY"
-		// from := "0x7b74c19124a9ca92c6141a2ed5f92130fc2791f2"
-		// to := "0x6ef149e870e4f7c67f98601ddc58461e79d9db0e"
-		// value := "32748.669"
-		// status := 20
-		// blockNum := int64(5644709)
-		// hash := "0x8da3c24bca1c4e2c17ad900be64d728e7d825a790b68138358cbe7f7dcece0f2"
-		// blocktime := time.Now()
-		// tx := BTx{ Asset:asset,  Fromaddr: from, Toaddr: to, Value: value, Status: status, Blocknum: blockNum, Txhash: hash, Blocktime: blocktime, Createtime: time.Now() }
-		// pgEngine.InsertOne(tx)
+
 	}
 }
